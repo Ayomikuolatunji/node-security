fd2564abb43db34db55c9f6ba53114659fb180ea
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-26 20:35:17 +0100

    using helemt

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const https=require("https")
 const fs=require("fs")
 const express=require("express")
+const helmet=require("helmet")
 const  path = require('path')
 
 const app=express()
+// app.use(helmet())
 
 
 
 
-
-app.get("/secret",()=>{
+app.get("/secret",(req,res)=>{
    return res.send("your personal secret is 42")
 })
 
@@ -19,10 +20,14 @@ app.get("/",(req,res)=>{
 
 
 
-https.createServer({
-  key: fs.readFileSync("key.pem"),
-  cert:fs.readFileSync("private.pem")
-},app)
-.listen(8080,()=>{
+// https.createServer({
+//   key: fs.readFileSync("key.pem"),
+//   cert:fs.readFileSync("private.pem")
+// },app)
+// .listen(8080,()=>{
+//   console.log("App is running on localhost 8080")
+// })
+
+app.listen(8080,()=>{
   console.log("App is running on localhost 8080")
 })

fa92cf644ff022b0645ff2432d6c6d3bba471c86
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-26 21:40:31 +0100

    gogle auth2 auth

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const fs=require("fs")
 const express=require("express")
 const helmet=require("helmet")
 const  path = require('path')
+const passport=require("passport")
+const {Strategy}=require("passport-google-oauth20")
+require("dotenv").config()
 
 const app=express()
 app.use(helmet())
+app.use(passport.initialize())
 
 
 const ClientId="149317685876-tc8an50m1h707e1d7d31j7pinlqjkp9g.apps.googleusercontent.com"
@@ -18,9 +22,21 @@ const config={
 }
 
 
+app.use(new Strategy({
+    callbackURL:"/auth/google/callback",
+    ClientId:config.ClientId,
+    clientSecret:config.ClientSecretKey
+},()=>{
+  
+}))
+
 app.get("/secret",(req,res)=>{
    return res.send("your personal secret is 42")
 })
+app.get("/auth/google",(req,res)=>{})
+app.get("/auth/google/callback",(req,res)=>{})
+app.get("/auth/logout",(req,res)=>{})
+
 
 app.get("/",(req,res)=>{
   res.sendFile(path.join(__dirname, "public","index.html"))
